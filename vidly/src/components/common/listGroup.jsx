52096887c671f929132d039644f7be6c46e94ff9
Владимир Ливинский <xDamneDx@Gmail.com>
2020-04-14 17:47:28 +0300

    In the process of passing 'Routing' chapter

--- vidly/src/components/common/listGroup.jsx
+++ vidly/src/components/common/listGroup.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const ListGroup = ({ items, selectedItem, textProperty, valueProperty }) => {
+const ListGroup = ({ items, selectedItem, textProperty, valueProperty, onItemSelect }) => {
   return (
     <ul className='list-group'>
       {items.map(item => (
         <li
           key={item[valueProperty]}
           className={item === selectedItem ? 'list-group-item active' : 'list-group-item clickable'}
-          onClick={() => props.onItemSelect(item)}
+          onClick={() => onItemSelect(item)}
         >{item[textProperty]}
         </li>))}
     </ul>

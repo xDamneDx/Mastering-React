dbc84115c6bae93ce33000e20b408c89812c549c
Владимир Ливинский <xDamneDx@Gmail.com>
2020-05-11 16:48:01 +0300

    In the process of passing 'Advanced Topics' chapter

---
+++ react-advanced/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Movie from './hoc/Movie';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Movie />
+  );
+}
+
+export default App;
